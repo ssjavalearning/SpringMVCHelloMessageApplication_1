7536db75329288f63f6e0b1ccd061f5c12c4946a
suraj singh <surajtech003@gmail.com>
2018-07-10 01:31:09 +0530

    first commit

---
+++ src/com/suraj/spring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.suraj.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public ModelAndView hello() {
+		return new ModelAndView("hello","helloMessage","welcome to spring mvc app");
+	}
+}
